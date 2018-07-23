b4454017763b2d1a73ddf8c2c4f3060fcea99130
F_jobs <846031560@qq.com>
2018-07-23 09:21:51 +0800

    第一次 model

---
+++ dao/database.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose');
+require("./model/user")
+require("./model/mendianguanli")
+require("./model/shangpinguanli")
+require("./model/fuwuguanli")
+require("./model/chongwuguanli")
+var dbURI = 'mongodb://localhost/ai';
+mongoose.connect(dbURI);
+mongoose.connection.on('connected', function () {
+  console.log('Mongoose connected to ' + dbURI);
+});
